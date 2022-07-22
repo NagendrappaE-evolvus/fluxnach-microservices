83a6c98e05070b18344fbfc6ee5d203e7cec42a4
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2022-07-22 13:31:30 +0530

    chnages

---
+++ common-service/src/main/java/io/paycorp/fluxnach/commonservice/config/IntegrationConfiguration.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package io.paycorp.fluxnach.commonservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author nagendrappae
+ *
+ */
+@Configuration
+@Slf4j
+@ImportResource({ "classpath*:application-context.xml" })
+public class IntegrationConfiguration {
+
+}
---
+++ outward-payment-server/src/main/java/io/paycorp/fluxnach/outwardpaymentserver/processor/PreProcessor.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package io.paycorp.fluxnach.outwardpaymentserver.processor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author nagendrappae
+ *
+ */
+@Component
+@Slf4j
+public class PreProcessor implements Processor{@Override
+	public void process(Exchange exchange) throws Exception {
+
+	
+	
+	}
+
+}

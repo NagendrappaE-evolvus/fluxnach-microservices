2d183e2286e5f24d2d3e1b5cb8fa9758b97ff52f
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2022-07-28 19:01:55 +0530

    done

--- entity-model/src/main/java/io/paycorp/fluxnach/entity/FpdPaymentOut.java
+++ entity-model/src/main/java/io/paycorp/fluxnach/entity/FpdPaymentOut.java
@@ -28,7 +28,7 @@ public class FpdPaymentOut {
 
 	private String outBatchRef;
 
-	private int sponBankSeq;
+	private int spoBankSeq;
 
 	private long payBankSeq;
 
@@ -52,11 +52,11 @@ public class FpdPaymentOut {
 
 	private BigDecimal claimAmt;
 
-	private String payAccName;
+	private String payAcName;
 
-	private String payAccType;
+	private String payAcType;
 
-	private String payAccNum;
+	private String payAcNum;
 
 	private String payBankCode;
 
@@ -66,9 +66,9 @@ public class FpdPaymentOut {
 
 	private BigDecimal settledAmt;
 
-	private Date settledDtttime;
+	private Date settledDttime;
 
-	private Date receivedDtttime;
+	private Date recvdDttime;
 
 	private Date claimDate;
 
@@ -82,32 +82,32 @@ public class FpdPaymentOut {
 
 	private String ddsFileName;
 
-	private String orgSeqNum;
+	private String orgSeq;
 
 	private String fileId;
 
-	private Long settledSession;
-	
+	private Long settleSession;
+
 	private String payStatus;
 
-    private String txnStatusCode;
+	private String qStatus;
 
 	private String ldgrFolioNum;
 
-	private String userName;
+	private String usrName;
 
-	private String userNum;
+	private String usrNum;
 
 	private String prodType;
 
-	private String consumerRefNumber;
+	private String consRefNum;
 
 	private String cbsHoldRef;
 
-	private String settledPhxDtttime;
-	
-	private String cbsHoldReferenceNumber;
-	
+	private String settledPhxDttime;
+
+	private String cbsHoldRefNum;
+
 	private String txnType;
 
 	private String adhaar;
@@ -115,19 +115,21 @@ public class FpdPaymentOut {
 	private BigDecimal aadhaarNum;
 
 	private String customRsnDesc;
-	
+
 	private Date RespDate;
-	
+
 	private String clientCode;
 
 	private String benefAccType;
 
 	private String mndRefNum;
 
-	private String usrField1;
-	
-	private Date usrField2;
-	
+	private String usrFld1;
+
+	private Date usrFld2;
+
+	private String usrFld3;
+
 	private String checkSum;
 
 	private String itemSeq;
@@ -137,43 +139,41 @@ public class FpdPaymentOut {
 
 	@Transient
 	private String returnDesc;
-	
 
 	@Transient
 	private String schemeCode;
-	
+
 	@Id
 	@Transient
 	private int qCount;
-	
+
 	@Transient
 	@Id
 	private String filler;
-	
+
 	@Id
 	@Transient
 	private BigDecimal amount;
-	
+
 	@Id
 	@Transient
 	private Integer settlementStatus;
-	
-	@Id
-	@Transient
-	private String respDate; 
 
 	@Id
 	@Transient
-	private String rsnDescOne; 
+	private String respDate;
 
 	@Id
 	@Transient
-	private String rsnDescTwo; 
+	private String rsnDescOne;
 
+	@Id
+	@Transient
+	private String rsnDescTwo;
 
 	public void setUserNameWithPrdType() {
-		String name = this.getProdType() + "-" + this.userName;
-		this.userName = name.substring(0, Math.min(name.length(), 20));
+		String name = this.getProdType() + "-" + this.usrName;
+		this.usrName = name.substring(0, Math.min(name.length(), 20));
 
 	}
 
